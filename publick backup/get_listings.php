573d71a2a01482d29f93e9c576aedf8c3b8a7196
wendynkirote8 <wendynkiroteg@gmailcom>
2025-03-29 18:59:16 +0300

    deleted old back up

--- publick backup/get_listings.php
+++
@@ -1,9 +0,0 @@
-<?php
-require '../config/db_connect.php';
-
-$stmt = $pdo->query("SELECT * FROM listings ORDER BY rating DESC");
-$listings = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-header('Content-Type: application/json');
-echo json_encode($listings);
-?>
